22b0e34a88251492407d1136eb1806df2c63e31d
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2023-01-08 04:19:48 -0700

    chore python black init files

--- SimpleBlog/asgi.py
+++ SimpleBlog/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SimpleBlog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SimpleBlog.settings")
 
 application = get_asgi_application()
--- SimpleBlog/wsgi.py
+++ SimpleBlog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SimpleBlog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SimpleBlog.settings")
 
 application = get_wsgi_application()
