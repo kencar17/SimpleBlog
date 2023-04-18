361d09a6dd3b29f7cf74688c6028a566f8afd531
carmichaelk3@mymacewan.ca <carmichaelk3@mymacewan.ca>
2023-04-17 20:28:36 -0600

    chore add static root

--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from simple_blog import settings
 
 urlpatterns = [
     path("blog-site-admin", admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path("api/", include("apps.blog.urls")),
     path("api/", include("apps.comments.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
